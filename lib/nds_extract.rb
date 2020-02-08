82916fa62a058d23f7052ea8743d4ac72770f817
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-08 14:50:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,11 +75,11 @@ def gross_per_studio(collection)
     while index < collection.size do
       studio = collection[index][:studio]
       gross = collection[index][:worldwide_gross]
-  # if !total_worldwide_gross[studio]
+    if !total_worldwide_gross[studio]
       total_worldwide_gross[studio] = gross
-  # else
-    # total_worldwide_gross[gross] += gross
-    # end
+    else
+      total_worldwide_gross[gross] += gross
+    end
     index += 1
   end
   total_worldwide_gross
